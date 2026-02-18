65ca562be2c5e025eaad58ce1b19bda9220e558b
edenbarkan <eden.barkan@gmail.com>
2026-02-18 16:13:47 +0200

    feat: add Terraform infrastructure with S3 backend

---
+++ terraform/outputs.tf
@@ -0,0 +1,14 @@
+output "instance_public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.app_server.public_ip
+}
+
+output "instance_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.app_server.public_dns
+}
+
+output "application_url" {
+  description = "URL to access the application"
+  value       = "http://${aws_instance.app_server.public_ip}:${var.app_port}"
+}
